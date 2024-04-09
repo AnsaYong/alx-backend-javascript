479afc96a876f6600a3b830c6e5f2626dd549562
Ansahmbom <ansahmbomyong@gmail.com>
2024-04-09 19:19:27 +0200

    updated 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Simulating an API call that resolves after 1 second
     setTimeout(() => {
       resolve('Response from API');

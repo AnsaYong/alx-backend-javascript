1f5da7aa0c815fe7f72a2b314e6d6ae1ccdb3bbd
Ansahmbom <ansahmbomyong@gmail.com>
2024-06-03 18:55:09 +0300

    4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+// Create a server object
+const app = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body "Hello Holberton School!"
+  res.end('Hello Holberton School!');
+});
+
+// The server object listens on port 1245
+app.listen(1245);
